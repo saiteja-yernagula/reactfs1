400b3c6cf7eb6ce5d9982c3e178b2b09dfbd89ab
saiteja-yernagula <saiteja.yernagula@gmail.com>
2026-05-25 11:40:27 +0530

    x

--- src/components/Doctorcard.jsx
+++ src/components/Doctorcard.jsx
@@ -1,13 +1,13 @@
 import './styles.css'
-function Doctorcard(props) {
+function Doctorcard({name,specialization,gender}) {
   return (
     <div className='doctorcard'>
         <div>
         <img src="https://cdn-icons-png.flaticon.com/512/387/387561.png" width='100' height='100' alt="" />
         </div>
-        <h1>{props.name}</h1>
-        <div>{props.specialization}</div>
-        <p>{props.gender}</p>
+        <h1>{name}</h1>
+        <div>{specialization}</div>
+        <p>{gender}</p>
         <button>View more</button>
     </div>
   )
