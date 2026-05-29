5aea35c0275efbb8e9b874b7ac356150c1115523
saiteja-yernagula <saiteja.yernagula@gmail.com>
2026-05-29 11:51:38 +0530

    dfg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,36 +9,12 @@ function Home({newdoctor}) {
     let [search,setSearch]=useState('')
     let [specialization,setSpecialization]=useState('')
 
-     function fetchdata(){
-      
-     let data = [
-                {
-                    id: 1,
-                    name: "Teja",
-                    age: 26,
-                    gender: "Male",
-                    specialization: "Muscles",
-                    salary: 7000000
-                },
-                {
-                    id: 2,
-                    name: "Sam",
-                    age: 26,
-                    gender: "Male",
-                    specialization: "Bones",
-                    salary: 4000000
-                },
-                {
-                    id: 3,
-                    name: "Anu",
-                    age: 25,
-                    gender: "Female",
-                    specialization: "Heart",
-                    salary: 5000000
-                }
-            ]
+   async  function fetchdata(){
+
+            let val=await fetch('https://doc-back.onrender.com/doctors')
+            let finaldata=await val.json()
          
-            setDoctors(data)
+            setDoctors(finaldata)
 
      }
     useEffect(()=>{
