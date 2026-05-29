682939e3c12874472cc8931530595cb03f31e313
saiteja-yernagula <saiteja.yernagula@gmail.com>
2026-05-29 12:09:25 +0530

    erer

--- src/components/Doctordetails.jsx
+++ src/components/Doctordetails.jsx
@@ -1,48 +1,35 @@
-import React from 'react'
+import  { useEffect,useState } from 'react'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 function Doctordetails() {
     let {id}=useParams()
-      let data = [
-                {
-                    id: 1,
-                    name: "Teja",
-                    age: 26,
-                    gender: "Male",
-                    specialization: "Muscles",
-                    salary: 7000000
-                },
-                {
-                    id: 2,
-                    name: "Sam",
-                    age: 26,
-                    gender: "Male",
-                    specialization: "Bones",
-                    salary: 4000000
-                },
-                {
-                    id: 3,
-                    name: "Anu",
-                    age: 25,
-                    gender: "Female",
-                    specialization: "Heart",
-                    salary: 5000000
-                }
-            ]
-            let doctor=data.find((val)=>{
-                return val.id==id
-            })
-        console.log(doctor);
-        
+    let [doctor,setDoctor]=useState(null)
+
+   async function fetchdata(){
+    let val=await axios.get('https://doc-back.onrender.com/doctors')
+    console.log(val.data )
+
+       let doc=val.data.find((val)=>{
+           return val.id==id
+       })
+       setDoctor(doc)
+
+       console.log(doc)
+   }
+      
+     useEffect(()=>{
+        fetchdata()
+       },[]) 
             
   return (
     <div>
     <h1>Doctordetails</h1>
-    <h1>{doctor.name}</h1>
-    <h1>{doctor.gender}</h1>
-    <h1>{doctor.specialization}</h1>
-    <h1>{doctor.salary}</h1>
-    <h1>{doctor.age}</h1>
+    <h1>{doctor && (<div>{doctor.name}</div>)}</h1>
+    <h1>{doctor && (<div>{doctor.gender}</div>)}</h1>
+    <h1>{doctor && (<div>{doctor.salary}</div>)}</h1>
+    <h1>{doctor && (<div>{doctor.specialization}</div>)}</h1>
+ 
     </div>
   )
 }
